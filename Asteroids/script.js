70bc5b055cf287e6796c4a817540a174a6ea5eb5
jeremmgn <monguin.jeremy@gmail.com>
2023-04-23 22:35:12 +0200

    Création de la classe Vaisseau et affichage du vaisseau au centre de l'écran

--- Asteroids/script.js
+++ Asteroids/script.js
@@ -9,3 +9,43 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Vaisseau {
+    constructor(x, y, taille) {
+        this.x = x;
+        this.y = y;
+        this.taille = taille;
+        this.angle = 90 / 180 * Math.PI;
+    }
+
+    draw() {
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(
+            this.x + 4 / 3 * this.taille * Math.cos(this.angle),
+            this.y - 4 / 3 * this.taille * Math.sin(this.angle)
+        );
+        ctx.lineTo(
+            this.x - this.taille * (2 / 3 * Math.cos(this.angle) + Math.sin(this.angle)),
+            this.y + this.taille * (2 / 3 * Math.sin(this.angle) - Math.cos(this.angle))
+        );
+        ctx.lineTo(
+            this.x - this.taille * (2 / 3 * Math.cos(this.angle) - Math.sin(this.angle)),
+            this.y + this.taille * (2 / 3 * Math.sin(this.angle) + Math.cos(this.angle))
+        );
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+const vaisseau = new Vaisseau(canvas.width / 2, canvas.height / 2, 12);
+
+function jeu() {
+    requestAnimationFrame(jeu);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    vaisseau.draw();
+}
+
+jeu();
